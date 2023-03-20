60ac893fe4fddbec598fa4d69bd82dfb5b9b1609
prog5 <prog5@it-delta.ru>
2023-03-20 16:33:53 +0300

    Added financial page

--- src/js/main.js
+++ src/js/main.js
@@ -412,4 +412,22 @@ window.onload = function () {
       }
     });
   }
+
+  const accordion = document.getElementsByClassName("detailed-accordion");
+
+  for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      const panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
 };
